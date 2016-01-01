4f0fc3376eb61e3077f6302d4492d62e354848c2
Simon <simon@Si-Book.local>
2016-01-01 23:30:03 +0000

    Initial views

---
+++ parts/View2.swift
@@ -0,0 +1,81 @@
+//
+//  View2.swift
+//  parts
+//
+//  Created by Simon on 01/01/2016.
+//  Copyright (c) 2016 Simon. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var cameraView: UIView!
+    
+    var captureSession : AVCaptureSession?
+    
+    var stillImageOutput :AVCaptureStillImageOutput?
+    
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer?.frame = cameraView.bounds
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error : NSError?
+        
+        var input = AVCaptureDeviceInput(device: backCamera, error: &error)
+        
+        if (error == nil && captureSession?.canAddInput(input) != nil)
+        {
+            captureSession?.addInput(input)
+        }
+        
+        stillImageOutput = AVCaptureStillImageOutput()
+        
+        stillImageOutput?.outputSettings = [AVVideoCodecKey :AVVideoCodecJPEG]
+        
+        if (captureSession?.canAddOutput(stillImageOutput) != nil)
+        {
+            captureSession?.addOutput(stillImageOutput)
+            previewLayer = AVCaptureVideoPreviewLayer(session : captureSession)
+            
+            previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+            
+            previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            
+            cameraView.layer.addSublayer(previewLayer)
+            
+            captureSession?.startRunning()
+            
+        }
+        
+    }
+
+}
